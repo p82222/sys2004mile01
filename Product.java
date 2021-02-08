2c55eed842a3e3e0c1e1d6997da6e6cbb3823b59
p82222 <42255490+p82222@users.noreply.github.com>
2021-02-08 16:07:18 -0500

    project code
    basic code for product class

---
+++ Product.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Product {
+
+
+    private String name;                     // name of the Product
+    private int id;                          // id of Product
+    private float price;                     // price of Product
+
+
+    /**
+     * Creates a new Product with the supplied attributes.
+     */
+    public Product(String name, int id, float price) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+    }
+
+    /**
+     * Get this Product's name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Get this Product's ID.
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Get this Product's price.
+     */
+    public float getPrice() {
+        return price;
+    }
+
+
+
+
+
+}
