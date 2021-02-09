33aa769652eef47f8f027bcfbde1074e87eb12d9
p82222 <42255490+p82222@users.noreply.github.com>
2021-02-09 01:46:15 -0500

    inventroy add set quantity

--- Inventory.java
+++ Inventory.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Inventory {
 
@@ -22,8 +23,7 @@ public class Inventory {
         this.type = type;
         this.quantity = quantity;
         this.product = product;
-        products.add(product);
-
+        this.products.add(product);
     }
 
     /**
@@ -52,44 +52,28 @@ public class Inventory {
      * Get the amount of stock for a given Product ID (Note: it is possible the Product does not exist in
      * the Inventory!)
      */
-    public int getQuantity(int  id) {
-        int num = 0;
-
-        for (Product p: this.products){
-            if (p.getId() != id){
-               continue;
-            } else{
-                num += 1;
-            }
+    public int getQuantity(int id) {
+        if (this.product.getId() == id){
+            return this.quantity;
         }
-        if (num == 0){
-            System.out.println("this product does not exist in the Inventory!");
-
-        }
-        return num;
-
-
-
+        System.out.println("Product does not exist in the Inventory!");
+        return 0;
     }
 
     /**
      * Add a specified amount of stock for a given Product to the inventory.
      */
     public void addQuantity(int id) {
-        boolean flag = false;
-        for (Product p: this.products){
-            if (p.getId() != id){
-                flag = true;
-                break;
-            }
+        if (this.product.getId() == id){
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("enter the stock amount");
+            int num = keyboard.nextInt();
+            this.quantity = num;
         }
-        if (flag == false){
-            if(!fullLoad){
-                courses.add(course);
-                if (courses.size() == 5)
-                    fullLoad = true;
-            }
-        }
+
+
+
+
     }
 
 
